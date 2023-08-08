eb9642cdf7baf9ee9a314d33f03f33c06067bc70
Arpita-K1610 <126654567+Arpita-K1610@users.noreply.github.com>
2023-08-08 21:03:29 +0530

    Add files via upload

--- login.js
+++ login.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mysql = require("mysql2");
+const bcrypt = require("bcrypt");
 const path = require("path");
 
 const app = express();
-const port = 3000; 
+const port = 3000; // Replace with your desired port number
 
-
+// Create a MySQL connection pool
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "Niteesh@1995",
-  database: "your_database_name",
+  database: "loginDB",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
@@ -20,32 +21,74 @@ const pool = mysql.createPool({
 // Parse incoming JSON data
 app.use(bodyParser.json());
 
-
+// Serve static files (including your HTML files)
 app.use(express.static(path.join(__dirname, "login")));
 
-// Endpoint to handle form submissions
-app.post("/submit", (req, res) => {
-  // Access form data from the request body
-  const formData = req.body;
-  const { name, email, password } = formData;
+// Endpoint to handle user login
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
 
-  
-  pool.query(
-    "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-    [name, email, password],
-    (error, results) => {
-      if (error) {
-        console.error("Error:", error);
+  // Fetch the user from the database based on the provided email
+  pool.query("SELECT * FROM users WHERE email = ?", [email], (error, results) => {
+    if (error) {
+      console.error("Error:", error);
+      return res.status(500).json({ success: false, message: "Internal server error" });
+    }
+
+    if (results.length === 0) {
+      // User with the provided email does not exist
+      return res.json({ success: false, message: "Login failed. User not found." });
+    }
+
+    const user = results[0];
+
+    // Compare the provided password with the hashed password stored in the database
+    bcrypt.compare(password, user.password, (err, passwordMatch) => {
+      if (err) {
+        console.error("Error:", err);
         return res.status(500).json({ success: false, message: "Internal server error" });
       }
 
-      
-      console.log("User added to the database:", results);
+      if (passwordMatch) {
+        // Password matches, login successful
+        return res.json({ success: true, message: "Login successful!" });
+      } else {
+        // Password does not match, login failed
+        return res.json({ success: false, message: "Login failed. Please check your credentials." });
+      }
+    });
+  });
+});
 
-      
-      res.json({ success: true, message: "Form submitted successfully!" });
+// Endpoint to handle user signup (registration)
+app.post("/signup", (req, res) => {
+  const { name, email, password } = req.body;
+
+  // Hash the user's password before saving it to the database
+  bcrypt.hash(password, 10, (err, hashedPassword) => {
+    if (err) {
+      console.error("Error:", err);
+      return res.status(500).json({ success: false, message: "Internal server error" });
     }
-  );
+
+    // Perform database insertion
+    pool.query(
+      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+      [name, email, hashedPassword],
+      (error, results) => {
+        if (error) {
+          console.error("Error:", error);
+          return res.status(500).json({ success: false, message: "Internal server error" });
+        }
+
+        // Handle successful database insertion
+        console.log("User added to the database:", results);
+
+        // Send a success response
+        res.json({ success: true, message: "User registered successfully!" });
+      }
+    );
+  });
 });
 
 // Start the server
